a8a89a899540585c88804ce6a1a3a019a5fb2a7c
sammiee5311 <56215891+sammiee5311@users.noreply.github.com>
2021-02-09 22:13:41 +0900

    update

--- 2d_practice/python/main.py
+++ 2d_practice/python/main.py
@@ -1,17 +1,42 @@
 import socket
 import random
+import torch
 from time import sleep
+from model import Model
+
+
+class Test:
+    def __init__(self):
+        self.model = Model(11, 256, 3)
+        self.model.load_state_dict(torch.load('./model/model.pth'))
+
+    def get_action(self, model, state):
+        final_choice = [0, 0, 0]
+        cur_state = torch.tensor(state, dtype=torch.float)
+        prediction = model(cur_state)
+        choice = torch.argmax(prediction).item()
+        final_choice[choice] = 1
+
+        return final_choice
 
 
 IP, PORT = '', 12345
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP, PORT))
+test = Test()
+received_data = None
 
 while True:
     sleep(0.5)
-    start_pos = [0, 0, 0]
-    start_pos[random.randint(0,2)] = random.randint(-1,1)
-    pos_string = ','.join(map(str, start_pos))
+    if received_data:
+        state = list(map(int, received_data.split(',')))
+        choice = test.get_action(test.model, state)
+        pos_string = ','.join(map(str, choice))
+        print(state)
+    else:
+        start_pos = [1, 0, 0]
+        pos_string = ','.join(map(str, start_pos))
+
     print(pos_string)
 
     s.sendall(pos_string.encode('UTF-8'))
